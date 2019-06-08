01e47019317ecf65888387a12ce82d047ed3926b
Himani Popli <himaniheenu@gmail.com>
2019-06-08 16:52:10 +0530

    Simple Linear Regression Added

---
+++ Simple Linear Regression.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 16:14:29 2019
+
+@author: Himani Popli
+"""
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+dataset=pd.read_csv('Salary_Data.csv')
+X=dataset.iloc[:, :-1].values
+Y=dataset.iloc[:,1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=1/3,random_state=0)
+
+# Fitting simple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,Y_train)
+
+# Predicting the test set results
+Y_pred=regressor.predict(X_test)
