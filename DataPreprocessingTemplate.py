aa488a0dc17f4e7fc8b227b87c24d3011c0c27bc
Himani Popli <himaniheenu@gmail.com>
2019-06-10 01:15:43 +0530

    Data preprocessing template

---
+++ DataPreprocessingTemplate.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Importing lib
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing dataset
+dataset=pd.read_csv('Data.csv')
+X=dataset.iloc[:, :-1].values
+Y=dataset.iloc[:,3].values
+
+# Splitting dataset into training and test sets
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
+
+"""# Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)"""
